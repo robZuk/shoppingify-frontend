4b547be4d6eae06db4639772c1ad2224b2fb7f7d
Robert <robert.aleks.zuk@gmail.com>
2023-09-29 13:14:17 +0200

    ref code

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        // target: "https://shoppingify-backend-61bb.onrender.com",
+        target: "https://shoppingify-backend-61bb.onrender.com",
         // target: "http://localhost:5000",
         changeOrigin: true,
         //
