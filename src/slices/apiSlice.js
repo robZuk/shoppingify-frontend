e6de67cac8d3c3f52bbbbb2f64de2c4ae2ea171a
Robert <robert.aleks.zuk@gmail.com>
2023-09-29 16:59:07 +0200

    ref3

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -1,6 +1,8 @@
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 const API_URL = import.meta.env.VITE_BASE_URL;
-const baseQuery = fetchBaseQuery({ baseUrl: API_URL });
+const baseQuery = fetchBaseQuery({
+  baseUrl: "https://shoppingify-backend-61bb.onrender.com",
+});
 
 export const apiSlice = createApi({
   baseQuery,
