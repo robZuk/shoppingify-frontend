4f879bdccbe776da399fe3cdfa82f4f7615c8f84
Robert <robert.aleks.zuk@gmail.com>
2023-09-29 14:57:24 +0200

    ref code

--- src/constans.js
+++ src/constans.js
@@ -1,6 +1,6 @@
-// const API_URL = import.meta.env.VITE_BASE_URL;
+const API_URL = import.meta.env.VITE_BASE_URL;
 
-export const PRODUCTS_URL = "/api/products";
-export const USERS_URL = "/api/users";
-export const LISTS_URL = "/api/lists";
-export const CATEGORIES_URL = "/api/categories";
+export const PRODUCTS_URL = API_URL + "/api/products";
+export const USERS_URL = API_URL + "/api/users";
+export const LISTS_URL = API_URL + "/api/lists";
+export const CATEGORIES_URL = API_URL + "/api/categories";
