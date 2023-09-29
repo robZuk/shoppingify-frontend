7c62be07395356ccc672a6bd6c8b23fe910ee69f
Robert <robert.aleks.zuk@gmail.com>
2023-09-29 14:50:29 +0200

    ref code

--- vite.config.js
+++ vite.config.js
@@ -5,17 +5,21 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": {
-        target: "https://shoppingify-backend-61bb.onrender.com",
-        changeOrigin: true,
-        secure: false,
-        // rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-      // "/api": {
-      //   // target: "https://shoppingify-backend-61bb.onrender.com",
-      //   target: "http://localhost:5000",
-      //   changeOrigin: true,
-      // },
+      "/api": "https://shoppingify-backend-61bb.onrender.com",
+      "/uploads": "https://shoppingify-backend-61bb.onrender.com",
     },
+    //   proxy: {
+    //     // "/api": {
+    //     //   target: "https://shoppingify-backend-61bb.onrender.com",
+    //     //   changeOrigin: true,
+    //     //   secure: false,
+    //     //   // rewrite: (path) => path.replace(/^\/api/, ""),
+    //     // },
+    //     // "/api": {
+    //     //   // target: "https://shoppingify-backend-61bb.onrender.com",
+    //     //   target: "http://localhost:5000",
+    //     //   changeOrigin: true,
+    //     // },
+    //   },
   },
 });
